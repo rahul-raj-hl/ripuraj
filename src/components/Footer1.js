7822d22bdcb23f566920b0fa5e2ca10bae55569e
srivastavarahul442 <srivastavarahul442@gmail.com>
2025-04-12 14:43:13 +0530

    fixed: bug for footer

--- src/components/Footer1.js
+++ src/components/Footer1.js
@@ -3,33 +3,49 @@ import React from "react";
 
 const Footer1 = () => {
   return (
-    <div className="relative w-full overflow-hidden">
-      {/* Background Image */}
-      <div className="h-40%">
-      <Image
-        src="/footer_1.jpg" // background with coins
-        alt="footer background"
-        layout="responsive"
-        width={500}
-        height={120}
-        objectFit="cover"
-        quality={100}
-        className="z-0 "
-      />
+    <div>
+      {/* Desktop View with Background and 10.png */}
+      <div className="relative w-full overflow-hidden">
+        {/* Background Image */}
+        <div className="h-40%">
+          <Image
+            src="/footer_1.jpg"
+            alt="footer background"
+            layout="responsive"
+            width={500}
+            height={120}
+            objectFit="cover"
+            quality={100}
+            className="z-0"
+          />
+        </div>
+
+        {/* Centered Product Image */}
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div className="w-4/9 md:w-4/8 lg:w-4/9">
+            <Image
+              src="/10_1.png"
+              alt="footer product"
+              layout="responsive"
+              width={500}
+              height={400}
+            />
+          </div>
+        </div>
       </div>
 
-      {/* Centered Product Image */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="w-4/9 md:w-4/8 lg:w-4/9 px-4">
+      {/* Mobile View with only 11.png (no background) */}
+      {/* <div className=" md:hidden">
+        <div className="w-100%">
           <Image
-            src="/10.png" // product group image
-            alt="footer product"
+            src="/11.png"
+            alt="footer product mobile"
             layout="responsive"
             width={500}
             height={400}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
