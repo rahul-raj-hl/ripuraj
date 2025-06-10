cdf78ac2a5c09719c0f4e7f15584bbcddacacdb2
Rahul Srivastava <118062330+srivastavarahul442@users.noreply.github.com>
2025-06-10 11:02:31 +0530

    added logout feat. (#70)

--- src/components/DashboardLogin.js
+++ src/components/DashboardLogin.js
@@ -2,38 +2,42 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { changeLoggedInUser } from "./utils/loggedInUserSlice";
 import { useRouter } from "next/router";
+import { useHydration } from "./hooks/useHydration";
 
 const DashboardLogin = () => {
-
-    const dispatch = useDispatch();
-    const router = useRouter();
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState(false);
 
+  const hydrated = useHydration();
+  if (!hydrated) return null; // or a loading spinner
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setErrorMsg(false)
+    setErrorMsg(false);
     // Handle login logic here
-    if(userId === "gold@ripuraj" && password === "gold@ripuraj"){
-        dispatch(changeLoggedInUser(true))
-        router.push("/dashboard");
-
+    if (userId === "gold@ripuraj" && password === "gold@ripuraj") {
+      dispatch(changeLoggedInUser(true));
+      router.push("/dashboard");
+    } else {
+      setErrorMsg(true);
     }
-    else{
-        setErrorMsg(true)
-    }
-    
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
       <div className="w-full max-w-sm bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-center text-2xl font-semibold mb-6 text-black">Admin Login</h2>
+        <h2 className="text-center text-2xl font-semibold mb-6 text-black">
+          Admin Login
+        </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="userId" className="block text-gray-700">Enter ID</label>
+            <label htmlFor="userId" className="block text-gray-700">
+              Enter ID
+            </label>
             <input
               type="text"
               id="userId"
@@ -45,7 +49,9 @@ const DashboardLogin = () => {
             />
           </div>
           <div className="mb-5">
-            <label htmlFor="password" className="block text-gray-700">Enter Password</label>
+            <label htmlFor="password" className="block text-gray-700">
+              Enter Password
+            </label>
             <input
               type="password"
               id="password"
@@ -56,7 +62,9 @@ const DashboardLogin = () => {
               required
             />
           </div>
-          {errorMsg && <p className="text-red-500 mb-6">Invalid Id & Password</p>}
+          {errorMsg && (
+            <p className="text-red-500 mb-6">Invalid Id & Password</p>
+          )}
           <button
             type="submit"
             className="w-full py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
