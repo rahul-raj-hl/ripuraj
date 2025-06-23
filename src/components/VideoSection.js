f59953fdf94e4088e58e190dc63bb9906961b5df
Nandan Kumar <109342663+nandanSrivastava@users.noreply.github.com>
2025-06-23 13:13:45 +0530

    updated hindi video in videosection (#81)

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -44,7 +44,7 @@ const VideoSection = () => {
         ) : (
           <iframe
             className="h-full w-full rounded-2xl"
-            src="https://www.youtube.com/embed/MxFC0tv-bK4?si=knfu_c3sINFhExsq"
+            src="https://www.youtube.com/embed/BOKQClMzRM0?si=co7K8JkDkwclmPux"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
